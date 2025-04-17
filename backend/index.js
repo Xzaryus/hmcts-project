94a00bbac8104558233dbe9355434ea7ecd9f05d
Xzaryus <nicocodes@hotmail.com>
2025-04-17 10:56:25 +0100

    Fix backend conn

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 const app = express();
-const port = process.env.MYSQLPORT || 5000;
+const port = process.env.PORT || 5000;
 
 const cors = require('cors');
 app.use(cors({
